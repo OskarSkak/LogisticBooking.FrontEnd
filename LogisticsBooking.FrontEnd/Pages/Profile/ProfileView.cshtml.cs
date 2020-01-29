8da47a511b17d93b0ff2b6c14775b0bf046bba42
Casper Hansern <caspha17@gmail.com>
2020-01-29 21:57:10 +0100

    skjd

---
+++ LogisticsBooking.FrontEnd/Pages/Profile/ProfileView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LogisticsBooking.FrontEnd.Pages.Profile
+{
+    public class ProfileView : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
